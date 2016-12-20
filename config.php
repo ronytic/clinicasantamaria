ab1c3b9479d3432de4ab772209ca31e411c54244
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-12-20 01:16:41 -0400

    config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$title="Sistema Administrativo Clinica Santa maria";
+$siglas="";
+$directory="clinicasantamaria";//ej:carp/ sin el "/"
+$url="http://11.1.2.31/";//ej:http:"localhost/"
+$lema2="";
+$lema='Las Salud es nuestra prioridad';
+$direccion="Av. 6 de Agosto";
+$telefono="2860724";
+
+$ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
+$tp="D3W1N8E0E4B4B4";
+?>
