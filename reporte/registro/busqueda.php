07cc30b4184250bc814688294084aee7c80417f4
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-12-25 11:45:07 -0400

    Busqueda

--- reporte/registro/busqueda.php
+++ reporte/registro/busqueda.php
@@ -10,7 +10,18 @@ if (!empty($_POST)) {
     //print_r($_SESSION);
     $idusuario=$_SESSION['idusuario'];
 	
-	$pq=$prequirurgico->mostrarTodo("fechaconsulta LIKE '$fechaconsulta' and coddoctor=$idusuario and codpaciente IN(SELECT codpaciente FROM paciente WHERE nombres LIKE '%$nombres%' and paterno LIKE '%$paterno%' and materno LIKE '%$materno%' and ci LIKE '$ci%')");
+	$pq=$prequirurgico->mostrarTodo("fechaconsulta LIKE '$fechaconsulta' and codpaciente IN(SELECT codpaciente FROM paciente WHERE nombres LIKE '%$nombres%' and paterno LIKE '%$paterno%' and materno LIKE '%$materno%' and ci LIKE '$ci%')");
+    
+    $enlaces=array("Reporte Prequirugico"=>"prequirugico.php",
+                "Form.de Internación"=>"internacion.php",
+                "Hist. Clinica"=>"historiaclinica.php",
+                "Hoja Eva."=>"hojaevaluacion.php",
+                "Hoja Eva. 2"=>"hojaevaluacion2.php",
+                "Pap. Orden "=>"papeleta.php",
+                "Operación"=>"operacion.php",
+                "Epicrisis"=>"epicrisis.php",
+                "Hoja Enfermera"=>"enfermera.php",
+                );
     $datos=array();
     $i=0;
     foreach($pq as $p){$i++;
@@ -27,6 +38,48 @@ if (!empty($_POST)) {
         $datos[$i]['fechaconsulta']=$p['fechaconsulta'];
     }
 	$titulo=array("nombres"=>"Nombres","paterno"=>"Paterno","materno"=>"Materno","ci"=>"C.I.","celular"=>"Celular","sexo"=>"Sexo","estadocivil"=>"Estado Civil","fechaconsulta"=>"Fecha de Consulta");
-	listadoTabla($titulo,$datos,1,"","","",array("Revisar Reporte"=>"reporte.php"));
+	//listadoTabla($titulo,$datos,1,"","","",$enlaces);
 }
 ?>
+<table class="tablalistado">
+    <thead>
+	<tr class="cabecera">
+	<td>Nº</td>
+		<td>Nombres</td>
+		<td>Paterno</td>
+		<td>Materno</td>
+		<td>C.I.</td>
+		<td>Celular</td>
+		<td>Sexo</td>
+		<td>Estado Civil</td>
+		<td>Fecha de Consulta</td>
+		</tr>
+	</thead>
+    <?php $i=0;foreach($datos as $d){$i++;
+    ?>
+    <tr class="contenido">
+	    <td><?php echo $i;?></td>
+		<td><?php echo $d['nombres']?></td>
+		<td><?php echo $d['paterno']?></td>
+		<td><?php echo $d['materno']?></td>
+		<td><?php echo $d['ci']?></td>
+		<td><?php echo $d['celular']?></td>
+		<td><?php echo $d['sexo']?></td>
+		<td><?php echo $d['estadocivil']?></td>
+		<td><?php echo $d['fechaconsulta']?></td>
+        <td>
+            <!--<a href="prequirugico.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Reporte Prequirugico</a>
+            <a href="internacion.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Form.de Internación</a>-->
+            <a href="historiaclinica.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Hist. Clinica</a>
+            <a href="hojaevaluacion.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Hoja Eva.</a>
+            <a href="hojaevaluacion2.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Hoja Eva. 2</a>
+            <a href="papeleta.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Pap. Orden </a>
+            <a href="operacion.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Operación</a>
+            <a href="epicrisis.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Epicrisis</a>
+            <a href="enfermera.php?id=<?php echo $d['codprequirurgico']?>" class="boton" target="_blank">Hoja Enfermera</a>
+           
+        </td>
+		</tr>
+    <?php    
+    }?>
+</table>
