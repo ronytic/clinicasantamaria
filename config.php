6ff863b4c4accf6cf5e24b4d185df366a386c067
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-12-25 11:45:00 -0400

    Config

--- config.php
+++ config.php
@@ -9,5 +9,5 @@ $direccion="Av. 6 de Agosto";
 $telefono="2860724";
 
 $ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
-$tp="D3W1N8E0E4B4B4";
+//$tp="D3W1N8E0E4B4B4";
 ?>
